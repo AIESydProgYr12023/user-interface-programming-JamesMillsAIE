4761997b4bfd0723e339265ef30bd791346b064a
James Mills <james.mills@aie.edu.au>
2023-08-17 16:40:04 +1000

    GitLFS

---
+++ Tutorials/Assets/AIE11_Animation/Scripts/CharacterAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace AIE11_Animation
+{
+	[RequireComponent(typeof(Animator))]
+	public class CharacterAnimation : MonoBehaviour
+	{
+		private static int walkHash = Animator.StringToHash("IsWalking");
+		
+		private Animator animator;
+
+		private void Start()
+		{
+			animator = gameObject.GetComponent<Animator>();
+		}
+
+		private void Update()
+		{
+			animator.SetBool(walkHash, Input.GetKey(KeyCode.Space));
+		}
+	}
+}
